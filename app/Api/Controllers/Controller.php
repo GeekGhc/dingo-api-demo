dedbf567c2c90f3b24b5241bfb73082000dbf098
GeekGhc <ghcgavin@sina.com>
2017-08-17 18:49:03 +0800

    dingo api :sparkles:

---
+++ app/Api/Controllers/Controller.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Api\Controllers;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+
+}
---
+++ app/Api/Controllers/LessonsController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Api\Controllers;
+
+use Illuminate\Http\Request;
+
+class LessonsController extends Controller
+{
+    //
+}
