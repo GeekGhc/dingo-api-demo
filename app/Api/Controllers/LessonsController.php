b5db7d41c9590b4a55ae01b8c4aa377bd20b70ec
GeekGhc <ghcgavin@sina.com>
2017-08-22 23:35:34 +0800

    api 路由:boom:

--- app/Api/Controllers/LessonsController.php
+++ app/Api/Controllers/LessonsController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class LessonsController extends Controller
 {
-    //
+    public function index()
+    {
+        $lessons =  Lesson::all();
+
+        return $lessons;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1', function ($api) {
+    $api->group(['namespace' => 'App\Api\Controllers'], function ($api) {
+
+    });
+});
